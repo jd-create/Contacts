a8c35b781e8dd8b44d336382522f3be9659ab874
Jennifer <jenniferdemeijervandriel73@gmail.com>
2024-07-01 10:42:15 +0200

    added gitignore and contacts.maui

---
+++ Contacts.Maui/Views/ContactsPage.xaml.cs
@@ -0,0 +1,37 @@
+namespace Contacts.Maui.Views;
+
+using Contacts.Maui.Models;
+using System.Collections.ObjectModel;
+using Contact = Contacts.Maui.Models.Contact;
+
+public partial class ContactsPage : ContentPage
+{
+	public ContactsPage()
+	{
+		InitializeComponent();
+	
+	}
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        var contacts = new ObservableCollection<Contact>(ContactRepository.GetContacts());
+
+        listContacts.ItemsSource = contacts;
+    }
+
+
+    private async void listContacts_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+	{
+		//logic		
+		if (listContacts.SelectedItem != null)
+		{
+			await Shell.Current.GoToAsync($"{nameof(EditContactPage)}?Id={((Contact)listContacts.SelectedItem).ContactId}");
+		}
+	}
+
+    private void listContacts_ItemTapped(object sender, ItemTappedEventArgs e)
+    {
+        listContacts.SelectedItem = null;
+    }
+}
---
+++ Contacts.Maui/Views/EditContactPage.xaml.cs
@@ -0,0 +1,47 @@
+using Contacts.Maui.Models;
+using Contact = Contacts.Maui.Models.Contact;
+namespace Contacts.Maui.Views;
+
+[QueryProperty(nameof(ContactId),"Id")]
+public partial class EditContactPage : ContentPage
+{
+    private Contact contact;
+	public EditContactPage()
+	{
+		InitializeComponent();
+	}
+
+   
+
+    public string ContactId
+    {
+        set
+        {
+            contact = ContactRepository.GetContactById(int.Parse(value));
+            if (contact != null)
+            {
+                entryName.Text = contact.Name;
+                entryEmail.Text = contact.Email;
+                entryPhone.Text = contact.Phone;
+                entryAddress.Text = contact.Adress;
+            }
+        }
+
+    }
+
+    private void btnCancel_Clicked_1(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync("..");
+    }
+
+    private void btnUpdate_Clicked(object sender, EventArgs e)
+    {
+        contact.Name = entryName.Text;
+        contact.Email = entryEmail.Text;
+        contact.Phone = entryPhone.Text;
+        contact.Adress = entryAddress.Text;
+
+        ContactRepository.UpdateContact(contact.ContactId, contact);
+        Shell.Current.GoToAsync("..");
+    }
+}
