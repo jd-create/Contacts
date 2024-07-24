7ee93c8625b892ed1f1dbee0aa41f38463b6ef93
Jennifer <jenniferdemeijervandriel73@gmail.com>
2024-07-24 10:00:55 +0200

    added untracked files

---
+++ Contacts.Maui/ViewModels/ContactViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Contacts.Maui.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contact = Contacts.Maui.Models.Contact;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Contacts.Maui.ViewModels
+{
+    public partial class ContactViewModel : ObservableObject
+    {
+        private Contact contact;
+        public Contact Contact 
+        { 
+            get => contact;
+            set 
+            {
+                SetProperty(ref contact, value);
+            }
+        }
+
+        public ContactViewModel()
+        {
+           this.Contact = new Contact();
+        }
+
+        public void LoadContact(int contactId)
+        {
+            this.Contact = ContactRepository.GetContactById(contactId);
+        }
+
+        [RelayCommand]
+        public void SaveContact()
+        {
+            ContactRepository.UpdateContact(
+                this.Contact.ContactId, 
+                this.Contact);
+        }
+    }
+}
