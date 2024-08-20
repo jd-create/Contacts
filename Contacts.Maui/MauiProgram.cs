7f04e8b199b2ebed25180ea85eaa80a1dcf877b5
Jennifer <jenniferdemeijervandriel73@gmail.com>
2024-08-20 15:00:32 +0200

    persist data in SQLite

--- Contacts.Maui/MauiProgram.cs
+++ Contacts.Maui/MauiProgram.cs
@@ -7,6 +7,7 @@ using Contacts.UseCases;
 using Contacts.Maui.Views;
 using Contacts.Maui.ViewModels;
 using Contacts.Maui.Views_MVVM;
+using Contacts.Plugins.DataStore.SQLLite;
 
 
 namespace Contacts.Maui
@@ -28,7 +29,8 @@ namespace Contacts.Maui
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
-            builder.Services.AddSingleton<IContactRepository, ContactInMemoryRepository>();
+            //builder.Services.AddSingleton<IContactRepository, ContactInMemoryRepository>();
+            builder.Services.AddSingleton<IContactRepository, ContactSQLiteRepository>();
             builder.Services.AddSingleton<IViewContactsUseCase, ViewContactsUseCase>();
             builder.Services.AddSingleton<IViewContactUseCase, ViewContactUseCase>();
             builder.Services.AddTransient<IEditContactUseCase, EditContactUseCase>();
